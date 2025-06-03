8b43669f79037233195eb9ca7c40b73aea531e6d
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2025-06-04 01:59:07 +0200

    db helpers

---
+++ reset_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TARGET_DIR="."
+
+# find "$TARGET_DIR": starts the search from the specified directory.
+# -type f: only matches regular files.
+# -name "*.db": matches files ending with .db.
+# -exec rm -f {} \;: deletes each matched file.
+find "$TARGET_DIR" -type f -name "*.db" -exec rm -f {} \;
